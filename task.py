30c9c5cbc1d56360f3d77361d834fff485072c9c
Indrashis Mitra <indrashismitra2000@gmail.com>
2021-06-29 23:43:42 +0530

    Create task.py

---
+++ task.py
@@ -0,0 +1,5 @@
+  
+if input("have you completed your course yet? >_") == "yes":
+    print("congratulations!! well done. you've completed an extremely long-term goal!")
+else:
+    print("Buddy what are you doing?? FINISH THAT TASK NOW!!!")
