df17f777efa64a69404898d45aecea7c79f35bcf
Indrashis Mitra <indrashismitra2000@gmail.com>
2021-07-04 18:34:14 +0530

    Create healthy.py

---
+++ healthy.py
@@ -0,0 +1,8 @@
+print("health hack")
+
+print("How many glasses of water did you drink  today?")
+
+if int(input()) < 8:
+    print("go drink a glass of water....now!")
+else:
+    print("Congrats! you are hydrated!")
